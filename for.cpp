cfd3a9518bfdde14c0e1202c44d3e4eb5750b782
Sai Sankar Jena <saisankarjena4568@gmail.com>
2024-08-02 00:48:57 +0530

    Add files via upload

---
+++ for.cpp
@@ -0,0 +1,25 @@
+//Sai sankar jena
+//23070123112
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i=0;i<10;i++)
+    {
+        cout<<"Hello sir"<<endl;
+    }
+    return 0;
+}
+//PS C:\Users\asus\Desktop\cds> cd "c:\Users\asus\Desktop\cds\" ; if ($?) { g++ for.CPP -o for } ; if ($?) { .\for }
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
+//Hello sir
---
+++ while.cpp
@@ -0,0 +1,18 @@
+
+//SAI SANKAR JENA
+//23070123112
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 10) {
+        cout << i << " ";
+        ++i;
+    }
+    cout << endl; // Print a newline after the loop
+
+    return 0;
+}
+//PS C:\Users\asus\Desktop\cds> cd "c:\Users\asus\Desktop\cds\" ; if ($?) { g++ while.cpp -o while } ; if ($?) { .\while }
+//1 2 3 4 5 6 7 8 9 10 
